61ef81071455e9bc7543301249fd62c9927563b7
pyhton-delevoper <viktorov.vadimka@gmail.com>
2023-02-08 22:47:00 +0700

    add minimal value

--- hexlet_django_blog/asgi.py
+++ hexlet_django_blog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hexlet_django_project project.
+ASGI config for hexlet_django_blog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hexlet_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hexlet_django_blog.settings')
 
 application = get_asgi_application()
